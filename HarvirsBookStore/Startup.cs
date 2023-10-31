ceaa6b8a73444cb6915202237889a85d16f05e21
W0824657 <W0824657@MYSCC.CA>
2023-10-31 17:39:28 -0400

    commit

--- HarvirsBookStore/Startup.cs
+++ HarvirsBookStore/Startup.cs
@@ -56,7 +56,7 @@ namespace HarvirsBookStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
