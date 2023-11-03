4fc37f36021dd9b0f179e9c804d1fc95a83af367
W0824657 <W0824657@myscc.ca>
2023-11-02 20:37:25 -0400

    Modify SP_Call.cs file and upddate read me

--- HarvirsBooks.DataAccess/Repository/SP_Call.cs
+++ HarvirsBooks.DataAccess/Repository/SP_Call.cs
@@ -10,7 +10,7 @@ using System.Linq;
 
 namespace HarvirsBooks.DataAccess.Repository
 {
-    public class SP_Call : SP_CallBase1, ISP_Call
+    public class SP_Call :  ISP_Call
     {
         // access the database
         private readonly ApplicationDbContext _db;
