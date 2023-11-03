c0ccb1742673336becd34b750d03810441c47137
W0824657 <W0824657@myscc.ca>
2023-11-02 21:42:41 -0400

    commit all

---
+++ HarvirsBookStore/Areas/Customer/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarvirsBookStore.Areas.Customer.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // API calls here
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            // return NotFound();
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+    }
+}
