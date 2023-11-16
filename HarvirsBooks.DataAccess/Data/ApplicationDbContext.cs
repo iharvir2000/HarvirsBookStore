59fd286438a846746c26eaee58cb6be2f5d12221
W0824657 <W0824657@myscc.ca>
2023-11-16 17:29:10 -0500

    commit all

--- HarvirsBooks.DataAccess/Data/ApplicationDbContext.cs
+++ HarvirsBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -14,5 +14,8 @@ namespace HarvirsBookStore.DataAccess.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> coverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+
     }
 }
