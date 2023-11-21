671dfd032cd8bbd6bd656ca55e91207173cab980
W0824657 <W0824657@myscc.ca>
2023-11-21 14:12:34 -0500

    add CoverTypeRepository update readme

--- HarvirsBooks.Models/CoverType.cs
+++ HarvirsBooks.Models/CoverType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace HarvirsBooks.Models
 {
     class CoverType
     {
+        public class Category
+        {
+            [Key]
+
+            public int Id { get; set; }
+
+            [Display(Name = "Category Name")]
+            [Required]
+            [MaxLength(50)]
+
+            public string Name { get; set; }
+        }
     }
 }
